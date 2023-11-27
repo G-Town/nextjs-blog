93c550540b13fe504fd6b5f0ef56ae3dab04ce87
G-Town <gerryarriagajr@gmail.com>
2023-11-27 16:37:30 -0600

    server error debug - enabled next-auth function route

--- src/app/api/comments/route.js
+++ src/app/api/comments/route.js
@@ -26,26 +26,26 @@ export const GET = async (req) => {
 };
 
 // CREATE A COMMENT
-// export const POST = async (req) => {
-//   const session = await getAuthSession();
+export const POST = async (req) => {
+  const session = await getAuthSession();
 
-//   if (!session) {
-//     return new NextResponse(
-//       JSON.stringify({ message: "Not Authenticated!" }, { status: 401 })
-//     );
-//   }
+  if (!session) {
+    return new NextResponse(
+      JSON.stringify({ message: "Not Authenticated!" }, { status: 401 })
+    );
+  }
 
-//   try {
-//     const body = await req.json();
-//     const comment = await prisma.comment.create({
-//       data: { ...body, userEmail: session.user.email },
-//     });
+  try {
+    const body = await req.json();
+    const comment = await prisma.comment.create({
+      data: { ...body, userEmail: session.user.email },
+    });
 
-//     return new NextResponse(JSON.stringify(comment, { status: 200 }));
-//   } catch (err) {
-//     console.log(err);
-//     return new NextResponse(
-//       JSON.stringify({ message: "Something went wrong!" }, { status: 500 })
-//     );
-//   }
-// };
+    return new NextResponse(JSON.stringify(comment, { status: 200 }));
+  } catch (err) {
+    console.log(err);
+    return new NextResponse(
+      JSON.stringify({ message: "Something went wrong!" }, { status: 500 })
+    );
+  }
+};
